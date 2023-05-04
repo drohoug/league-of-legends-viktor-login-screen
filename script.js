9142fa5980f496bd534b38dfffb0f80af0d38fff
MachineHerald <109990820+drohoug@users.noreply.github.com>
2023-05-04 20:35:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,28 @@ const handleChange = () => {
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 inputs.forEach((input) => input.addEventListener('input', handleChange));
+
+window.onload = function() {
+  var images = [
+    "/images/wallpaper.jpg",
+    "/images/wallpaper2.jpg",
+    "/images/wallpaper3.jpg",
+    "/images/wallpaper4.png",
+    "/images/wallpaper5.jpg",
+    "/images/wallpaper6.jpg",
+    "/images/wallpaper7.jpg",
+    "/images/wallpaper8.jpg",
+    "/images/wallpaper9.jpg",
+    "/images/wallpaper10.jpg",
+    "/images/wallpaper11.jpg",
+    "/images/wallpaper12.jpg",
+    "/images/wallpaper13.jpg",
+    "/images/wallpaper14.jpg"
+    
+  ];
+
+  var randomIndex = Math.floor(Math.random() * images.length);
+
+  var backgroundElement = document.getElementById("background-image");
+  backgroundElement.style.setProperty('background-image', "url('" + images[randomIndex] + "')");
+};
